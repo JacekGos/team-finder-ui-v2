8c6f2e71d23b21a34436f7d7ec1c1053ddf4edca
JacekGos <jacekg93@gmail.com>
2023-11-26 11:33:47 +0100

    Updated nav links styles

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import './topbar.css';
 import { Container, Navbar, Nav, Form, Button } from "react-bootstrap";
 import { RiTeamLine } from "react-icons/ri";
+import { TbMenu2 } from "react-icons/tb";
+
 
 
 export interface IAppProps {
@@ -11,25 +13,36 @@ export default function Topbar(props: IAppProps) {
 
     return (
         <Navbar collapseOnSelect expand="lg" className="position-fixed position-absolute w-100 top-0 left-0">
-            <Container fluid className='d-flex justify-content-center'>
-                <Navbar.Brand className='mb-0' href="/">
-                    <p className="brand-title">
-                        <RiTeamLine />
-                        Team-Finder
-                    </p>
-                </Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto" />
-                    <Nav>
-                        <Nav.Item>
-                            <Nav.Link eventKey={1}>
+            <Navbar.Brand className='mb-0' href="/">
+                <p className="brand-title">
+                    <RiTeamLine />
+                    Team-Finder
+                </p>
+            </Navbar.Brand>
+            <Navbar.Toggle>
+                <TbMenu2 color='#62707a'/>
+            </Navbar.Toggle>
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto" />
+                <Nav>
+                    <Nav.Item>
+                        <Nav.Link eventKey={1} href="/events">
+                            <div className='nav-item-not-filled'>
                                 Wydarzenia
-                            </Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
+                            </div>
+                        </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+                <Nav>
+                    <Nav.Item>
+                        <Nav.Link eventKey={2} href="/signin" >
+                            <div className='nav-item-filled'>
+                                Zaloguj
+                            </div>
+                        </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     );
 }
