d8f01da895c78aad1589c7285d9fe6add0f217d0
JacekGos <jacekg93@gmail.com>
2023-11-27 19:52:24 +0100

    Updated advanced modal styles

--- src/components/filter/modal/AdvancedModalContent copy.tsx
+++ src/components/filter/modal/AdvancedModalContent copy.tsx
@@ -15,86 +15,69 @@ export default function AdvancedModalContent(props: IAdvancedModalContentProps) 
 
     return (
         <>
-            <Modal.Header className="modal-base" style={{ borderBottom: '1px solid #103861' }}>
-                <Modal.Title>Więcej filtrów</Modal.Title>
-                <button className="input-base rounded-circle d-flex align-items-center p-2" onClick={() => props.handleClose(false)}>
-                    <TfiClose />
-                </button>
-            </Modal.Header >
             <Modal.Body className="modal-base">
                 <Form>
-                    <Form.Group style={{ borderBottom: 'solid 1px #103861' }}>
+                    <Form.Group className='modal-bottom-border'>
                         <Form.Label>
                             Zasięg
                         </Form.Label>
                         <Form.Range min={1} max={15} step={1} />
                         <div className="d-flex align-items-center justify-content-center">
-                            <Form.Control type="number" placeholder="5km" className="input-base input-search mb-3 w-50" />
+                            <Form.Control type="number" placeholder="5km" className="input-search mb-3 w-50" style={{ maxWidth: '50vh' }} />
                         </div>
                     </Form.Group>
-                    <Form.Group style={{ borderBottom: 'solid 1px #103861' }}>
+                    <Form.Group className='modal-bottom-border'>
                         <Form.Label>
                             Cena
                         </Form.Label>
-                        <div className="d-flex align-items-center">
+                        <div className="d-flex align-items-center justify-content-between">
                             <div className="d-flex flex-column align-items-center ps-3 pe-3">
                                 <Form.Text >min.</Form.Text>
-                                <Form.Control type="number" placeholder="0" className="input-base input-search mb-3 w-100" />
+                                <Form.Control type="number" placeholder="0" className="input-search mb-3 w-100" />
                             </div>
                             <div className="d-flex flex-column align-items-center ps-3 pe-3">
                                 <Form.Text >max.</Form.Text>
-                                <Form.Control type="number" placeholder="25" className="input-base input-search mb-3 w-100" />
+                                <Form.Control type="number" placeholder="25" className="input-search mb-3 w-100" />
                             </div>
                         </div>
                     </Form.Group>
-                    <Form.Group style={{ borderBottom: 'solid 1px #103861' }}>
+                    <Form.Group className='modal-bottom-border'>
                         <Form.Label>
                             Gracze
                         </Form.Label>
-                        <div className="d-flex align-items-center">
+                        <div className="d-flex align-items-center justify-content-between">
                             <div className="d-flex flex-column align-items-center ps-3 pe-3">
                                 <Form.Text >min.</Form.Text>
-                                <Form.Control type="number" placeholder="0" className="input-base input-search mb-3 w-100" />
+                                <Form.Control type="number" placeholder="0" className="input-search mb-3 w-100" />
                             </div>
                             <div className="d-flex flex-column align-items-center ps-3 pe-3">
                                 <Form.Text >max.</Form.Text>
-                                <Form.Control type="number" placeholder="25" className="input-base input-search mb-3 w-100" />
+                                <Form.Control type="number" placeholder="25" className="input-search mb-3 w-100" />
                             </div>
                         </div>
                     </Form.Group>
-                    <Form.Group >
+                    <Form.Group className='modal-bottom-border'>
                         <Form.Label>
                             Data
                         </Form.Label>
                         <div className="d-flex align-items-center justify-content-between ps-3">
                             <div className="d-flex flex-column align-items-center ">
                                 <Form.Text >od</Form.Text>
-                                <Form.Control type="date" defaultValue={new Date().toString()} className="input-base input-search mb-3 w-100" />
+                                <Form.Control type="date" defaultValue={new Date().toString()} className="input-search mb-3 w-100" />
                             </div>
                             <div className="d-flex flex-column align-items-center pe-3">
                                 <Form.Text >do</Form.Text>
-                                <Form.Control type="date" className="input-base input-search mb-3 w-100" />
+                                <Form.Control type="date" className="input-search mb-3 w-100" />
                             </div>
                         </div>
                     </Form.Group>
-                    {props.isFullscreen
-                        ?
-                        <Container className="position-fixed position-absolute bottom-0 end-0 d-flex align-items-center justify-content-center w-100"
-                            style={{ padding: '5px 0px 5px 10px', backgroundColor: '#0A1929', height: 70, borderTop: '1px solid #103861' }}>
-                            <button className="input-base ">
-                                Pokaż wydażenia
-                            </button>
-                        </Container>
-                        :
-                        <Container className="d-flex align-items-center justify-content-center w-100"
-                            style={{ padding: '5px 0px 5px 10px', backgroundColor: '#0A1929', height: 70, borderTop: '1px solid #103861' }}>
-                            <button className="input-base ">
-                                Pokaż wydażenia
-                            </button>
-                        </Container>
-                    }
+                    <div className="d-flex align-items-center justify-content-center  w-100 mt-3">
+                        <button className="btn-base btn-filled">
+                            Pokaż wydażenia
+                        </button>
+                    </div>
                 </Form>
-            </Modal.Body>
+            </Modal.Body >
         </>
     );
 }
--- src/components/filter/modal/FilterModal.tsx
+++ src/components/filter/modal/FilterModal.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { Modal } from 'react-bootstrap';
 import SimpleModalContent from './SimpleModalContent';
 import AdvancedModalContent from './AdvancedModalContent copy';
+import { TfiClose } from "react-icons/tfi";
+import { boolean } from 'yargs';
 
 export interface IFilterModalProps {
   show: boolean;
@@ -21,11 +23,19 @@ export default function FilterModal(props: IFilterModalProps) {
     }
   }
 
+
   return (
     <>
-      <Modal show={props.show} isFullscreen={props.isFullscreen}
+      <Modal show={props.show}
+        fullscreen={props.isFullscreen ? true : 'md-down'}
         style={{ zIndex: 1100 }}
       >
+        <Modal.Header className="modal-base modal-bottom-border">
+          <Modal.Title>Więcej filtrów</Modal.Title>
+          <button className="input-base rounded-circle d-flex align-items-center p-2" onClick={() => props.handleClose(false)}>
+            <TfiClose />
+          </button>
+        </Modal.Header >
         {getModalContent()}
       </Modal>
     </>
