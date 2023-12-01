888355466c238c26fc134aea1962d497f7eee4c2
Jacek Gos <jacekg93@gmail.com>
2023-12-01 14:53:49 +0100

    Updated picutres carousel styles in login page

--- src/components/auth/Pictures.tsx
+++ src/components/auth/Pictures.tsx
@@ -1,33 +1,48 @@
 import * as React from "react";
 import { Carousel } from "react-bootstrap";
 // import picture1 from '../../assets/sport1.png';
-import picture1 from "../../assets/sport1.png";
+import picture1 from "../../assets/sport1.jpg";
+import picture2 from "../../assets/sport2.png";
 
 export interface IPicturesProps {}
 
 export default function Pictures(props: IPicturesProps) {
-
-    const interval = 2000;
+  const interval = 2000;
 
   return (
     <div>
-      <Carousel>
-        <Carousel.Item interval={interval}>
-          <img src={picture1} alt="picture1" className="w-100" style={{borderRadius: 10}}/>
+      <Carousel fade={true}>
+        <Carousel.Item interval={interval} style={{ textAlign: "center" }}>
+          <img
+            src={picture1}
+            alt="picture1"
+            className="d-block w-100"
+            style={{ borderRadius: 10 }}
+          />
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={interval}>
-          <img src={picture1} alt="picture2" className="w-100" style={{borderRadius: 10}}/>
+        <Carousel.Item interval={interval} style={{ textAlign: "center" }}>
+          <img
+            src={picture2}
+            alt="picture2"
+            className="w-100"
+            style={{ borderRadius: 10 }}
+          />
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={interval}>
-          <img src={picture1} alt="picture3" className="w-100" style={{borderRadius: 10}}/>
+        <Carousel.Item interval={interval} style={{ textAlign: "center" }}>
+          <img
+            src={picture1}
+            alt="picture3"
+            className="d-block w-100"
+            style={{ borderRadius: 10 }}
+          />
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>
