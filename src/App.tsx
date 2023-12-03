977979d5e948476d58b9d390abdcac5088331ecb
JacekGos <jacekg93@gmail.com>
2023-12-03 09:29:41 +0100

    Added redirection for default url

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Topbar from "./components/topbar/Topbar";
 import { Container } from "react-bootstrap";
 import Filterbar from "./components/filter/Filterbar";
 import Events from "./components/events/Events";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes, redirect } from "react-router-dom";
 import Login from "./components/auth/Login";
 import MainLayout from "./layout/MainLayout";
 import Register from "./components/auth/Register";
@@ -35,16 +35,7 @@ function App() {
             <Route
               index
               element={
-                <div
-                  style={{
-                    marginTop: 60,
-                    maxHeight: "100%",
-                    backgroundColor: "#1D2125",
-                  }}
-                >
-                  <Filterbar />
-                  <Events />
-                </div>
+                <Navigate to="/events" />
               }
             />
             <Route
